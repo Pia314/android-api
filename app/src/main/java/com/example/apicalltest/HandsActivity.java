cd328d7d6897c68caec799671906962413e92639
Pia314 <pia.doepper@web.de>
2022-12-02 22:08:52 +0100

    cleaning some stuff

--- app/src/main/java/com/example/apicalltest/HandsActivity.java
+++ app/src/main/java/com/example/apicalltest/HandsActivity.java
@@ -16,13 +16,18 @@ package com.example.apicalltest;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -41,10 +46,32 @@ import com.google.mediapipe.solutions.hands.HandsResult;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import com.example.apicalltest.APIStructures.Message;
+import com.example.apicalltest.APIStructures.MessageOut;
+import com.example.apicalltest.ActionGestures.*;
 
 /** Main activity of MediaPipe Hands app. */
 public class HandsActivity extends AppCompatActivity {
   private static final String TAG = "HandsActivity";
+
+  private static double DISTANCE = 0.2;
+  private static double Z_DISTANCE = 0.3;
+
+  private boolean isSendingMessage = false;
+
+  private boolean isReceivingMessage = false;
+
+  boolean canRecognize = true;
+
+  private List<Float> list_z_coordinates=new ArrayList<Float>();
 
   private Hands hands;
   // Run the pipeline and the model inference on GPU or CPU.
@@ -254,13 +281,14 @@ public class HandsActivity extends AppCompatActivity {
   private void setupLiveDemoUiComponents() {
     Button startCameraButton = findViewById(R.id.button_start_camera);
     startCameraButton.setOnClickListener(
-        v -> {
-          if (inputSource == InputSource.CAMERA) {
-            return;
-          }
-          stopCurrentPipeline();
-          setupStreamingModePipeline(InputSource.CAMERA);
-        });
+            v -> {
+              if (inputSource == InputSource.CAMERA) {
+                return;
+              }
+              stopCurrentPipeline();
+              Toast.makeText(getApplicationContext(), "is Receiving Message is true", Toast.LENGTH_LONG).show();
+              setupStreamingModePipeline(InputSource.CAMERA);
+            });
   }
 
   /** Sets up core workflow for streaming mode. */
@@ -366,10 +394,202 @@ public class HandsActivity extends AppCompatActivity {
     Landmark wristWorldLandmark =
         result.multiHandWorldLandmarks().get(0).getLandmarkList().get(HandLandmark.WRIST);
     Log.i(
-        TAG,
-        String.format(
-            "MediaPipe Hand wrist world coordinates (in meters with the origin at the hand's"
-                + " approximate geometric center): x=%f m, y=%f m, z=%f m",
-            wristWorldLandmark.getX(), wristWorldLandmark.getY(), wristWorldLandmark.getZ()));
+            TAG,
+            String.format(
+                    "MediaPipe Hand wrist world coordinates (in meters with the origin at the hand's"
+                            + " approximate geometric center): x=%f m, y=%f m, z=%f m",
+                    wristWorldLandmark.getX(), wristWorldLandmark.getY(), wristWorldLandmark.getZ()));
+
+    if(isStartSharingPosition(result)){
+      list_z_coordinates.clear();
+      isSendingMessage = true;
+      //TODO Make it working
+      //stopCurrentPipeline();
+      if(canRecognize) {
+        //Toast.makeText(getApplicationContext(), "is Receiving Message is true", Toast.LENGTH_LONG).show();
+        canRecognize = false;
+      }
+      //
+     // postMessageToEveryone("myUsername", "message", "openableBy");
+    }
+
+    if(isReceivingPosition(result)){
+      list_z_coordinates.clear();
+      isReceivingMessage = true;
+      //TODO make ist working
+      //stopCurrentPipeline();
+      //Toast.makeText(getApplicationContext(), "is Receiving Message is true", Toast.LENGTH_LONG).show();
+      //retrieveMessage("username", "requester");
+      Log.d(
+              "test", "te5st");
+
+      if(canRecognize) {
+       // Toast.makeText(getApplicationContext(), "is Receiving Message is true", Toast.LENGTH_LONG).show();
+        canRecognize = false;
+      }
+    }
+  }
+
+  private boolean isStartSharingPosition(HandsResult result) {
+    // here i want to code if the start position (first three landmarks are together and changing z coordinates in the right direction)
+
+    //to access the landmarks
+    List<NormalizedLandmark> landmarkList = result.multiHandLandmarks().get(0).getLandmarkList();
+    // See here https://google.github.io/mediapipe/solutions/hands.html#hand-landmark-model
+    float[] thumb_tip = {landmarkList.get(4).getX(), landmarkList.get(4).getY(), landmarkList.get(4).getZ()};
+    float[] index_finger_tip = {landmarkList.get(8).getX(), landmarkList.get(8).getY(), landmarkList.get(8).getZ()};
+    float[] middle_finger_tip = {landmarkList.get(12).getX(), landmarkList.get(12).getY(), landmarkList.get(12).getZ()};
+    float[] ring_finger_tip = {landmarkList.get(16).getX(), landmarkList.get(16).getY(), landmarkList.get(16).getZ()};
+
+    // are three of the for point near together?
+    boolean threeFingersTogether = AreThreeFingersTogether(thumb_tip, index_finger_tip, middle_finger_tip) || AreThreeFingersTogether(thumb_tip, middle_finger_tip, ring_finger_tip);
+
+    if (threeFingersTogether) {
+      // if yes add mean of z coordinates to List
+      float mean_z_coordinates = (thumb_tip[2] + middle_finger_tip[2])/2;
+      list_z_coordinates.add(mean_z_coordinates);
+      myViewer("mean z coordinates" + mean_z_coordinates );
+    }
+
+    if (list_z_coordinates.size() >= 10) {
+      myViewer("diff z coordinates" + (list_z_coordinates.get(list_z_coordinates.size()-1) - list_z_coordinates.get(0)));
+      myViewer("last z coordinates" + list_z_coordinates.get(list_z_coordinates.size()-1) );
+      myViewer("first z coordinates" + list_z_coordinates.get(0));
+      if(((list_z_coordinates.get(list_z_coordinates.size()-1) - list_z_coordinates.get(0)) >= Z_DISTANCE) && (list_z_coordinates.get(list_z_coordinates.size()-1) >= list_z_coordinates.get(0))) {
+        myViewer("StartSharingPosition");
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean AreThreeFingersTogether(float[] finger1, float[] finger2, float[] finger3) {
+    float x_coordinate_difference_pairwise_sum = Math.abs(finger1[0] - finger2[0]) + Math.abs(finger1[0] - finger3[0]) + Math.abs(finger2[0] - finger3[0]);
+    float y_coordinate_difference_pairwise_sum = Math.abs(finger1[1] - finger2[1]) + Math.abs(finger1[1] - finger3[1]) + Math.abs(finger2[1] - finger3[1]);
+    float z_coordinate_difference_pairwise_sum = Math.abs(finger1[2] - finger2[2]) + Math.abs(finger1[2] - finger3[2]) + Math.abs(finger2[2] - finger3[2]);
+    boolean x_close = x_coordinate_difference_pairwise_sum <= DISTANCE;
+    boolean y_close = x_coordinate_difference_pairwise_sum <= DISTANCE;
+    boolean z_close = x_coordinate_difference_pairwise_sum <= DISTANCE;
+
+    boolean together = (x_close) && (y_close) && (z_close);
+
+    myViewer("x_close :" + x_close);
+    myViewer("y_close :" + y_close);
+    myViewer("z_close :" + z_close);
+    myViewer("together :" + together);
+
+    return together;
+  }
+
+  private boolean isReceivingPosition(HandsResult result) {
+    // here i want to code if receiving position (first three landmarks are together and changing z coordinates in the right (going closer to the screen) direction)
+
+    //to access the landmarks
+    List<NormalizedLandmark> landmarkList = result.multiHandLandmarks().get(0).getLandmarkList();
+    // See here https://google.github.io/mediapipe/solutions/hands.html#hand-landmark-model
+    float[] thumb_tip = {landmarkList.get(4).getX(), landmarkList.get(4).getY(), landmarkList.get(4).getZ()};
+    float[] index_finger_tip = {landmarkList.get(8).getX(), landmarkList.get(8).getY(), landmarkList.get(8).getZ()};
+    float[] middle_finger_tip = {landmarkList.get(12).getX(), landmarkList.get(12).getY(), landmarkList.get(12).getZ()};
+    float[] ring_finger_tip = {landmarkList.get(16).getX(), landmarkList.get(16).getY(), landmarkList.get(16).getZ()};
+
+    // are three of the for point near together?
+    boolean threeFingersTogether = AreThreeFingersTogether(thumb_tip, index_finger_tip, middle_finger_tip) || AreThreeFingersTogether(thumb_tip, middle_finger_tip, ring_finger_tip);
+
+    if (threeFingersTogether) {
+
+    }
+
+    if (threeFingersTogether) {
+      // if yes add mean of z coordinates to List
+      float mean_z_coordinates = (thumb_tip[2] + middle_finger_tip[2])/2;
+      list_z_coordinates.add(mean_z_coordinates);
+    }
+
+    if (list_z_coordinates.size() >= 10) {
+      myViewer("diff z coordinates" + (list_z_coordinates.get(list_z_coordinates.size()-1) - list_z_coordinates.get(0)));
+      myViewer("last z coordinates" + list_z_coordinates.get(list_z_coordinates.size()-1) );
+      myViewer("first z coordinates" + list_z_coordinates.get(0));
+      if ( true) { //(list_z_coordinates.get(list_z_coordinates.size()-1) - list_z_coordinates.get(0) >= Z_DISTANCE)  && ( list_z_coordinates.get(list_z_coordinates.size()-1) >= list_z_coordinates.get(0)) ){
+        myViewer("ReceivingPosition" );
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private void postMessageToEveryone(String myUsername, String message, String openableBy) {
+    Call<APIStructures.Message> call = RetrofitClient.getInstance().getMyApi().sendMessageToEveryone(myUsername, message, openableBy);
+    Log.d("d", call.request().toString());
+    call.enqueue(new Callback<APIStructures.Message>() {
+      @Override
+      public void onResponse(Call<APIStructures.Message> call, Response<APIStructures.Message> response) {
+        APIStructures.Message result = response.body();
+        Log.d("d", response.toString());
+        try {
+          myViewer("Message uploaded.");
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+
+      @Override
+      public void onFailure(Call<APIStructures.Message> call, Throwable t) {
+        myViewer("ERROR on upload.");
+      }
+    });
+  }
+
+  private void retrieveMessage(String username, String requester) {
+    Call<MessageOut> call = RetrofitClient.getInstance().getMyApi().getMessages(username, requester);
+    call.enqueue(new Callback<MessageOut>() {
+
+      @Override
+      public void onResponse(Call<MessageOut> call, Response<MessageOut> response) {
+        MessageOut result = response.body();
+        try {
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+
+      @Override
+      public void onFailure(Call<MessageOut> call, Throwable t) {
+        myViewer("ERROR");
+      }
+    });
+  }
+
+  public void myViewer(String str) {
+    // TODO: add View to render Information text. Toast is not working here
+    Log.d("d", str);
+  }
+  public void animateColor(String color) {
+    FrameLayout frameLayout = findViewById(R.id.michel);
+    Animation animation1 = new AlphaAnimation(0, 1); // Change alpha
+    animation1.setDuration(100); // duration - half a second
+    animation1.setInterpolator(new LinearInterpolator());
+    animation1.setRepeatMode(Animation.REVERSE);
+    animation1.setRepeatCount(1);
+    animation1.setAnimationListener(new Animation.AnimationListener() {
+      @Override
+      public void onAnimationStart(Animation animation) {
+
+      }
+
+      @Override
+      public void onAnimationEnd(Animation animation) {
+        frameLayout.setAlpha(0);
+      }
+
+      @Override
+      public void onAnimationRepeat(Animation animation) {
+
+      }
+    });
+    frameLayout.setAlpha(1);
+    frameLayout.setBackgroundColor(Color.parseColor(color));
+    frameLayout.startAnimation(animation1);
   }
 }
